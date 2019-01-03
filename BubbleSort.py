4b8c6d2fbf5601b247942e5941a39c47e5ba94e9
jayaramkapil <34901932+jayaramkapil@users.noreply.github.com>
2019-01-03 10:27:45 -0600

    Simple Sequential Search

---
+++ BubbleSort.py
@@ -0,0 +1,12 @@
+# Bubble sort
+
+def BubbleSort(mylist):
+    for passnum in range(len(mylist)-1,0,-1):
+        for i in range(passnum):
+            if mylist[i]>mylist[i+1]:
+                temp=mylist[i]
+                mylist[i]=mylist[i+1]
+                mylist[i+1]=temp
+    return mylist
+print(BubbleSort([54,26,93,17,77,31,44,55,20]))
+
